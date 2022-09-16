22a37805bead1dd6c29f54b765d0191b63cdaeba
S-i-o-n-i-s-t <metalljan@yandex.ru>
2022-09-16 19:34:31 +0300

    написали метод для вывода значений массива в консоль

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алкогитма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами
+void PrintArray(string[] Name)
+{
+    for (int pozition = 0; pozition < Name.Length; pozition++)
+    {
+        Console.Write($"{Name[pozition]} ");
+    }
+}
